e50145279bb7f9324884bbd4a905793acad789c9
Swathi Anand <slanand32@gmail.com>
2015-02-18 23:16:32 -0500

    edited articles model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
 
 	belongs_to :category
 
-	validate_presence_of :title, :content
+	validates_presence_of :title, :content
 
 	scope :active, where('active = ?', true)
   	scope :alphabetical, order('title')
